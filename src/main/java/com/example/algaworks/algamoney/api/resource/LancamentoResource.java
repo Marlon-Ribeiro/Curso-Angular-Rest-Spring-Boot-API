a984e663cb2707d160b121c6a4a98bee4de6b918
Marlon <marlon.ribeiro906@outlook.com>
2021-07-21 23:25:08 -0400

    Cadastrando o primeiro lançamento

--- src/main/java/com/example/algaworks/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algaworks/algamoney/api/resource/LancamentoResource.java
@@ -1,14 +1,16 @@
 package com.example.algaworks.algamoney.api.resource;
 
+import com.example.algaworks.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algaworks.algamoney.api.model.Lancamento;
 import com.example.algaworks.algamoney.api.repository.LancamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,9 +19,20 @@ public class LancamentoResource {
     @Autowired
     private LancamentoRepository lancamentoRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Lancamento> listar() {
         return lancamentoRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Lancamento>criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response){
+        Lancamento lancamentoSalvo= lancamentoRepository.save(lancamento);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response,lancamentoSalvo.getCodigo()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
     }
 
     @GetMapping("/{codigo}")
