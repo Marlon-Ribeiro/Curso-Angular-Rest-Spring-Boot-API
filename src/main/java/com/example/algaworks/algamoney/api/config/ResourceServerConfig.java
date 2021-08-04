c7d2a75d0704a97490e1623964adb9eab6349720
Marlon <marlon.ribeiro906@outlook.com>
2021-08-04 00:02:12 -0400

    Implementando com OAuth 2 e PassWord Flow

--- src/main/java/com/example/algaworks/algamoney/api/config/ResourceServerConfig.java
+++ src/main/java/com/example/algaworks/algamoney/api/config/ResourceServerConfig.java
@@ -1,17 +1,21 @@
 package com.example.algaworks.algamoney.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    @Override
+    @Autowired
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //user and password --> autenticao / Role--> autorizazao
         auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ROLE");
@@ -19,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
        http.authorizeRequests()
                .antMatchers("/categorias").permitAll()
                .anyRequest().authenticated()
@@ -28,6 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                .csrf().disable();
     }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.stateless(true);
+    }
+
 }
 
 
