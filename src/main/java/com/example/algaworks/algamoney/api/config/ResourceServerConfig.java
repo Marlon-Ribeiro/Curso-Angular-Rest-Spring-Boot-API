a250c922f02e2e6b2e2ee80535ef9a06eab301c6
Marlon-Ribeiro <marlon.ribeiro906@outlook.com>
2021-08-24 00:26:21 -0400

    Movendo o usuario para o banco de dados

--- src/main/java/com/example/algaworks/algamoney/api/config/ResourceServerConfig.java
+++ src/main/java/com/example/algaworks/algamoney/api/config/ResourceServerConfig.java
@@ -1,11 +1,12 @@
 package com.example.algaworks.algamoney.api.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -15,22 +16,20 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    @Autowired
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //user and password --> autenticao / Role--> autorizazao
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ROLE");
 
+    //{ @Autowired
+    //public void configure(AuthenticationManagerBuilder auth) throws Exception {
+      //  auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    //}
 
-    }
     @Override
     public void configure(HttpSecurity http) throws Exception {
-       http.authorizeRequests()
-               .antMatchers("/categorias").permitAll()
-               .anyRequest().authenticated()
-               .and()
-               .httpBasic().and()
-               .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-               .csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/categorias").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .csrf().disable();
     }
 
     @Override
@@ -38,6 +37,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         resources.stateless(true);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
-
-
