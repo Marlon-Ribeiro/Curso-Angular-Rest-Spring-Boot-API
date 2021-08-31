720b1a9adbd9691418edd984bbca2147a4e3028e
Marlon-Ribeiro <marlon.ribeiro906@outlook.com>
2021-08-31 00:48:09 -0400

    Adicionando novas permissões

--- src/main/java/com/example/algaworks/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algaworks/algamoney/api/resource/LancamentoResource.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -38,11 +39,13 @@ public class LancamentoResource {
     private MessageSource messageSource;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and hasAuthority('SCOPE_read')")
     public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
         return lancamentoRepository.filtrar(lancamentoFilter, pageable);
     }
 
     @GetMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and hasAuthority('SCOPE_read')")
     public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
         return this.lancamentoRepository.findById(codigo)
                 .map(lancamento -> ResponseEntity.ok(lancamento))
@@ -50,11 +53,13 @@ public class LancamentoResource {
     }
 
     @DeleteMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_REMOVER_LANCAMENTO') and hasAuthority('SCOPE_write')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long codigo){this.lancamentoRepository.deleteById(codigo);
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and hasAuthority('SCOPE_write')")
     public ResponseEntity<Lancamento>criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response){
         Lancamento lancamentoSalvo= lancamentoService.salvar(lancamento);
         publisher.publishEvent(new RecursoCriadoEvent(this, response,lancamentoSalvo.getCodigo()));
--- src/main/java/com/example/algaworks/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algaworks/algamoney/api/resource/PessoaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -17,25 +18,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaResource {
-        @Autowired
-        private PessoaRepository pessoaRepository;
-        @Autowired
+    @Autowired
+    private PessoaRepository pessoaRepository;
+    @Autowired
         private PessoaService pessoaService;
-        @Autowired
+    @Autowired
         private ApplicationEventPublisher publisher;
 
         @GetMapping
+        @PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and hasAuthority('SCOPE_read')")
         public List<Pessoa> listar(){
             return pessoaRepository.findAll();
         }
 
         @PostMapping
+        @PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and hasAuthority('SCOPE_write')")
         public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
             Pessoa pessoaSalva = pessoaRepository.save(pessoa);
             publisher.publishEvent(new RecursoCriadoEvent(this, response,pessoaSalva.getCodigo() ));
             return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
         }
     @GetMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and hasAuthority('SCOPE_read')")
     public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
             return  this.pessoaRepository.findById(codigo).map(pessoa ->
                     ResponseEntity.ok(pessoa)).
@@ -44,17 +48,20 @@ public class PessoaResource {
 
     @DeleteMapping("/{codigo}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasAuthority('ROLE_REMOVER_PESSOA') and hasAuthority('SCOPE_write')")
     public void remover(@PathVariable Long codigo) {
         this.pessoaRepository.deleteById(codigo);
     }
 
      @PutMapping("/{codigo}")
+     @PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and hasAuthority('SCOPE_write')")
     public ResponseEntity<Pessoa> atualizar (@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa){
             Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
             return ResponseEntity.ok(pessoaSalva);
      }
      @PutMapping("/{codigo}/ativo")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+     @ResponseStatus(HttpStatus.NO_CONTENT)
+     @PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and hasAuthority('SCOPE_write')")
     public void atualizarPropriedadeAtivo(@PathVariable long codigo, @RequestBody Boolean ativo){
             pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
      }
